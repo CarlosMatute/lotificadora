0acbd6ae9feac399de06efb1ad030bb3a9d8eea5
Carlos Matute <Carlos Matute@192.168.2.2>
2022-12-10 18:35:30 -0600

    eliminar residencial

--- lotificadora/app/Http/Controllers/controladorResidencialesApoyo.php
+++ lotificadora/app/Http/Controllers/controladorResidencialesApoyo.php
@@ -150,9 +150,11 @@ class controladorResidencialesApoyo extends Controller
         $residencial = Residenciale::find($id);
 
         $imagen = $residencial->imagen;
-        //File::delete('archivos/'.$nombre_archivo->url_archivo); este debo usar
-        unlink($imagen);
-
+        //File::delete('public/dist/img/'.$imagen); //desarrollo
+        //File::delete(public_path('dist/img').'archivos/'.$imagen); //produccion
+        unlink('dist/img/'.$imagen); // desarrollo
+        unlink('pruebaslotificadora/dist/img/'.$imagen); // produccion
+        
         $residencial->delete();
     }
 }
